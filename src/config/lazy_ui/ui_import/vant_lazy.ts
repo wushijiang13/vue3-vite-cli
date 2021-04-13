742bbb476d7322c850ae3fd4b5d048a6a557f3a2
wushijiang <wsj@rowenatech.com>
2021-04-13 14:45:37 +0800

    升级tsc版本修复vant引入特殊别名处理导致build后无法正常打开

--- src/config/lazy_ui/ui_import/vant_lazy.ts
+++ src/config/lazy_ui/ui_import/vant_lazy.ts
@@ -1,6 +1,5 @@
-//这里起别名是为了解决命名一直带来的引入冲突
-import { Button as vantButton } from 'vant';
+import { Button } from 'vant';
 import {app} from '../../vue/app'
 
 // @vue-ignore
-app.component(vantButton.name,vantButton);
+app.component(Button.name,Button);
