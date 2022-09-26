74c4f67b77759aca5a42f9381f4043bd22339ab4
wushijiang <wushijiang@huan.tv>
2022-09-26 18:35:37 +0800

    feat:upgrade template-vue3-ts-initial package version

--- packages/create/template-vue3-ts-initial/src/config/i18n/i18n.ts
+++ packages/create/template-vue3-ts-initial/src/config/i18n/i18n.ts
@@ -3,6 +3,7 @@ import {store} from '../../store/store'
 import {messages} from "./language_pack/i18n_message";
 
 let i18n=createI18n({
+    legacy: false,
     locale:store.state.languages,
     fallbackLocale:"en",
     messages,
